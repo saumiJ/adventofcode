f2b579f84edeecdfbb083356c4f86b4fcc51a812
sjoshi <saumitra.joshi@protonmail.com>
2022-12-02 14:55:02 +0100

    add solution to 2015.D01 and 2015.D02

---
+++ 2015/2015.01.py
@@ -0,0 +1,13 @@
+with open("input/2015.01.in", "r") as f:
+    lines = f.readlines()
+
+floor = 0
+first_basement_instruction_position = -1
+for line in lines:
+    for i, instruction in enumerate(line):
+        floor += (1 if instruction == "(" else (-1 if instruction == ")" else 0))
+        if floor == -1 and first_basement_instruction_position == -1:
+            first_basement_instruction_position = i + 1
+
+print(f"Target floor for Santa: {floor}\n"
+      f"Position of instruction that causes Santa to first enter the basement: {first_basement_instruction_position}")
---
+++ 2015/2015.02.py
@@ -0,0 +1,15 @@
+with open("input/2015.02.in", "r") as f:
+    lines = f.readlines()
+
+wrapping_paper_area = 0
+ribbon_length = 0
+for line in lines:
+    line = line.strip("\n")
+    l, w, h = (int(num_str) for num_str in line.split("x"))
+    a1, a2, a3 = l * w, w * h, h * l
+    p1, p2, p3 = 2 * (l + w), 2 * (w + h), 2 * (l + h)
+    volume = l * w * h
+    wrapping_paper_area += 2 * (a1 + a2 + a3) + min(a1, a2, a3)
+    ribbon_length += min(p1, p2, p3) + volume
+print(f"Total area of wrapping paper needed: {wrapping_paper_area} sq.ft.")
+print(f"Total length of ribbon needed: {ribbon_length} ft.")
