bdef2c94c0aaf717d067789fbdbf99097c6d28cb
sjoshi <saumitra.joshi@protonmail.com>
2022-12-02 14:05:21 +0100

    add solution to 2022.D02.P2

--- 2022/02/02-rockpaperscissors.py
+++ 2022/02/02-rockpaperscissors.py
@@ -2,44 +2,69 @@
 
 import functools
 
+from enum import Enum
 
-rck = (0, "rock    ")
-ppr = (1, "paper   ")
-scr = (2, "scissors")
+
+class Shape(Enum):
+    Rock = "A"
+    Paper = "B"
+    Scissors = "C"
+
+    def __init__(self, label):
+        self.label = label
+        self.id, self.score = (0, 1) if label == "A" else ((1, 2) if label == "B" else (2, 3))
+
+    def beats(self) -> "Shape":
+        return {
+            Shape.Rock: Shape.Scissors,
+            Shape.Paper: Shape.Rock,
+            Shape.Scissors: Shape.Paper
+        }[self]
+
+    def draws(self) -> "Shape":
+        return self
+
+    def loses_to(self) -> "Shape":
+        return {
+            Shape.Rock: Shape.Paper,
+            Shape.Paper: Shape.Scissors,
+            Shape.Scissors: Shape.Rock
+        }[self]
+
+
+class Strategy(Enum):
+    SHAPE = "SHAPE"
+    OUTCOME = "OUTCOME"
+
+    def get_shapes(self, input_str: str) -> (Shape, Shape):
+        first, second = input_str.split(" ")
+        op_shape = Shape(first)
+        if self is Strategy.SHAPE:
+            my_shape = {
+                "X": Shape.Rock,
+                "Y": Shape.Paper,
+                "Z": Shape.Scissors,
+            }[second]
+        elif self is Strategy.OUTCOME:
+            my_shape = {
+                "X": op_shape.beats(),
+                "Y": op_shape.draws(),
+                "Z": op_shape.loses_to()
+            }[second]
+        else:
+            raise NotImplementedError(self)
+        return op_shape, my_shape
 
 
 @functools.lru_cache(maxsize=None)
-def shape_to_shape_id(shape: str) -> (int, str):
-    """Convert shape-name to shape-index. Tool-indices: Rock = 0, Paper = 1, Scissors = 2"""
-    return {
-        "A": rck,
-        "X": rck,
-        "B": ppr,
-        "Y": ppr,
-        "C": scr,
-        "Z": scr,
-    }[shape]
-
-
-@functools.lru_cache(maxsize=None)
-def id_to_shape_score(shape_id: int) -> int:
-    """Convert shape-id to shape-score"""
-    return {
-        0: 1,
-        1: 2,
-        2: 3
-    }[shape_id]
-
-
-@functools.lru_cache(maxsize=None)
-def round_score(op: str, me: str) -> int:
+def round_score(shapes: (Shape, Shape)) -> int:
+    op, me = shapes
     outcome_score_matrix = [
         [3, 6, 0],
         [0, 3, 6],
         [6, 0, 3],
     ]
-    (op_id, op_n), (me_id, me_n) = shape_to_shape_id(op), shape_to_shape_id(me)
-    score = outcome_score_matrix[op_id][me_id] + id_to_shape_score(me_id)
+    score = outcome_score_matrix[op.id][me.id] + me.score
     return score
 
 
@@ -47,10 +72,13 @@ def round_score(op: str, me: str) -> int:
 with open("02.in", "r") as f:
     lines = f.readlines()
 
-# play with given strategy
-total_score = 0
+part_1_strategy = Strategy.SHAPE
+part_2_strategy = Strategy.OUTCOME
+total_score_part_1 = 0
+total_score_part_2 = 0
 for line in lines:
     line = line.strip("\n")
-    (_op, _me) = line.split(" ")
-    total_score += round_score(_op, _me)
-print(f"Total score following strategy-guide: {total_score}")
+    total_score_part_1 += round_score(part_1_strategy.get_shapes(line))
+    total_score_part_2 += round_score(part_2_strategy.get_shapes(line))
+print(f"Part-1 score: {total_score_part_1}\n"
+      f"Part-2 score: {total_score_part_2}\n")
