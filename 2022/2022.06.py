68e8ffac0a8122f50fa4a8a77769b8524dc4e9d8
sjoshi <saumitra.joshi@protonmail.com>
2022-12-06 09:20:49 +0100

    refactor

--- 2022/2022.06.py
+++ 2022/2022.06.py
@@ -4,19 +4,19 @@ with open("input/2022.06.in", "r") as f:
     signal = f.readline()
 
 
-def find_start_of_packet_marker_position(sgnl: str, n: int) -> int:
+def find_marker_position(sgnl: str, sequence_size: int) -> int:
     last_n = list()
     for i, c in enumerate(sgnl):
         last_n.append(c)
         len_last_n = len(last_n)
-        if len_last_n < n:
+        if len_last_n < sequence_size:
             continue
-        elif len_last_n > n:
+        elif len_last_n > sequence_size:
             last_n.pop(0)
         if len(last_n) == len(set(last_n)):
             return i + 1
     raise ValueError("start of packet marker not found!")
 
 
-print(f"Start-of-packet marker position: {find_start_of_packet_marker_position(signal, n=4)}")
-print(f"Start-of-message marker position: {find_start_of_packet_marker_position(signal, n=14)}")
+print(f"Start-of-packet marker position: {find_marker_position(signal, sequence_size=4)}")
+print(f"Start-of-message marker position: {find_marker_position(signal, sequence_size=14)}")
